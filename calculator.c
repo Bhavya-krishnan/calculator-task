bae9e6c52266327119f8994dbb1b1e9e7a719b59
Bhavya <6bhavyakrishnan@gmail.com>
2026-03-10 23:17:26 +0530

    fixed issues

--- calculator.c
+++ calculator.c
@@ -53,32 +53,83 @@ int main()
 
         printf("Enter your choice: ");
         scanf("%d",&choice);
-
+        float a,b,result;
+       
         if(choice==1)
         {
             printf("Addition");
+printf("Enter two numbers: ");
+       scanf("%f %f",&a,&b);
 
-            
+            result = add(a,b);
+            printf("Result = %f",result);
         }
         else if(choice==2)
         {
             printf("Subtraction");
+printf("Enter two numbers: ");
+       scanf("%f %f",&a,&b);
+
+                result = subtraction(a,b);
+               printf("Result = %f",result);
+
         }
         else if(choice==3)
         {
             printf("Multiplication");
+printf("Enter two numbers: ");
+       scanf("%f %f",&a,&b);
+
+                result = multiplication(a,b);
+                printf("Result = %f",result);
+
         }
         else if(choice==4)
         {
             printf("Division");
+printf("Enter two numbers: ");
+       scanf("%f %f",&a,&b);
+
+            if(b==0)
+    {
+        printf("Error: Division by zero\n");
+    }
+    else
+    {
+        result = division(a,b);
+        printf("Result = %.2f\n",result);
+    }
+
         }
         else if(choice==5)
         {
             printf("Exponent");
+
+            float base,exp,powerResult;
+
+    printf("Enter base and exponent: ");
+    scanf("%f %f",&base,&exp);
+
+    powerResult = power(base,exp);
+
+    printf("Result = %f",powerResult);
         }
         else if(choice==6)
         {
             printf("Square Root");
+
+            double num,sqrtResult;
+
+    printf("Enter a number: ");
+    scanf("%lf",&num);
+
+    sqrtResult = squareroot(num);
+
+    if(sqrtResult != -1)
+    {
+        printf("Square root = %.2lf\n",sqrtResult);
+    }
+
         }
         else if(choice==7)
         {
