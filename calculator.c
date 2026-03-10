203b60b104459ab5f5bb5ea292e7f2ae43ab7587
Bhavya <6bhavyakrishnan@gmail.com>
2026-03-10 22:05:30 +0530

    created calculator menu

---
+++ calculator.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int choice;
+    do
+    {
+        printf("Desktop calculator");
+        printf("1. Addition\n");
+        printf("2. Subtraction\n");
+        printf("3. Multiplication\n");
+        printf("4. Division\n");
+        printf("5. Exponent\n");
+        printf("6. Square Root\n");
+        printf("7. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d",&choice);
+
+        if(choice==1)
+        {
+            printf("Addition");
+        }
+        else if(choice==2)
+        {
+            printf("Subtraction");
+        }
+        else if(choice==3)
+        {
+            printf("Multiplication");
+        }
+        else if(choice==4)
+        {
+            printf("Division");
+        }
+        else if(choice==5)
+        {
+            printf("Exponent");
+        }
+        else if(choice==6)
+        {
+            printf("Square Root");
+        }
+        else if(choice==7)
+        {
+            printf("Exiting program");
+        }
+        else
+        {
+            printf("Invalid choice");
+        }
+
+    }while(choice!=7);
+
+    return 0;
+}
